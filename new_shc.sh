6f39246ccb979a05a33679d536b41a2e5653feae
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2017-02-24 02:03:43 +0000

    refactor

--- new_shc.sh
+++ new_shc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p run
-sed -e s/SSS/$1/ -e s/TTT/$2/ all-SSS-TTT.shc > run/all-$1-$2.shc
+sed -e s/SSS/$1/ -e s/TTT/$2/ template/all-SSS-TTT.shc > run/all-$1-$2.shc
 echo Created run script: run/all-$1-$2.shc >&2
-echo -e "To submit it, simply use:\n\nqsub run/all-$1-$2.shc\n" >&2
+echo -e "To submit it, simply use:\n\ncd run; qsub all-$1-$2.shc\n" >&2
 
