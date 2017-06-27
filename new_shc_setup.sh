bc095cf52635c17edeffc1a466caafb6b47a9357
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2017-06-27 12:14:54 +0000

    monotrans scripts and tempaltes

--- new_shc_setup.sh
+++ new_shc_setup.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-if [ -n $1 ];
+if [ -z $3 ]
 then
-    echo "Usage: $0 SETUPNAME"
+    echo "Usage: $0 setupname srclang tgtlang"
+    echo
     echo "Known setup names: "
     cd templates;
-    ls allsteps*
+    ls allsteps*|cut -d. -f1|tr "\n" " "
+    echo
+    echo
+    echo "Known languages: "
+    cd ../treebanks
+    ls|cut -c1-2|sort -u|tr "\n" " "
+    echo
 else
     s=$1
-
+    S=$2
+    T=$3
     mkdir -p run
-    sed -e s/SSS/$1/ -e s/TTT/$2/ templates/header-SSS-TTT.shc > run/$s-$1-$2.shc
-    cat templates/$s.shc >> run/$s-$1-$2.shc
-    echo Created run script: run/$s-$1-$2.shc >&2
-    echo -e "To submit it, simply use:\n\ncd run; qsub $s-$1-$2.shc\n" >&2
+    sed -e s/SSS/$S/ -e s/TTT/$T/ templates/header-SSS-TTT.shc > run/$s-$S-$T.shc
+    cat templates/$s.shc >> run/$s-$S-$T.shc
+    echo Created run script: run/$s-$S-$T.shc >&2
+    echo -e "To submit it, simply use:\n\ncd run; qsub $s-$S-$T.shc; cd ..\n" >&2
 
 fi
 
