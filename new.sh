ee891d1e26c6e67b52121526ec7c394c9f3e74a0
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2017-06-30 23:48:05 +0000

    generics instead of hardcoded

--- new.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p run
-sed -e s/SSS/$1/ -e s/TTT/$2/ templates/all-SSS-TTT.sh > run/all-$1-$2.sh
-chmod a+x run/all-$1-$2.sh
-
--- new_shc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p run
-sed -e s/SSS/$1/ -e s/TTT/$2/ templates/all-SSS-TTT.shc > run/all-$1-$2.shc
-echo Created run script: run/all-$1-$2.shc >&2
-echo -e "To submit it, simply use:\n\ncd run; qsub all-$1-$2.shc\n" >&2
-
