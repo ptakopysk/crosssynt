68ccbf6768755b0676be4ddf976602e85a26b117
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2018-01-02 01:44:51 +0000

    missing stuff (useful only locally)

---
+++ source.bash
@@ -0,0 +1,12 @@
+function GOLD() {
+    echo treebanks/$1-ud-dev.conllu
+}
+
+function OUTX() {
+    echo outputs/en-$1.transmoses-w2v-xtag.tlt.dev.conllu
+}
+
+function OUTS() {
+    echo outputs/en-$1.sup-w2v.tlt.dev.conllu
+}
+
