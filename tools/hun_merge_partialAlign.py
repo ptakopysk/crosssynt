24ce451d76c87193ef33a50d657a0da790dc5608
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2018-02-06 02:11:55 +0000

    sentence ids to sentences

--- tools/hun_merge_partialAlign.py
+++ tools/hun_merge_partialAlign.py
@@ -22,10 +22,13 @@ tgt_add = 0
 with open(batchf) as batch:
     for batch_line in batch:
         (srcf, tgtf, alnf) = batch_line.split()
-        with open(f(alnf)) as aln:
-            for aln_line in aln:
-                (srcl, tgtl, score) = aln_line.split()
-                print(int(srcl)+src_add, int(tgtl)+tgt_add, score, sep='\t')
+        try:
+            with open(f(alnf)) as aln:
+                for aln_line in aln:
+                    (srcl, tgtl, score) = aln_line.split()
+                    print(int(srcl)+src_add, int(tgtl)+tgt_add, score, sep='\t')
+        except FileNotFoundError:
+            print("WARN: skipping non-existent alignment file", f(alnf), file=sys.stderr)
         src_add += l(srcf)
         tgt_add += l(tgtf)
 
